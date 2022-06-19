cde6878999d590e7a6e1795ab030d5f380f4035a
Markus Borer <markus.borer@adesso.ch>
2022-06-19 12:30:09 +0200

    Initial checkin

--- cleancode/src/main/java/ch/markusborer/romannumerals/Translate.java
+++ cleancode/src/main/java/ch/markusborer/romannumerals/Translate.java
@@ -2,69 +2,41 @@ package ch.markusborer.romannumerals;
 
 public class Translate {
 
-    private int result;
-
     public int translate(String roman) {
+        int result = 0;
         for (int i = 0; i < roman.length(); i++) {
             String symbol = roman.substring(i, i + 1);
             String nextSymbol = "";
             if (roman.length() > i + 1) {
                 nextSymbol = roman.substring(i + 1, i + 2);
             }
-            if (symbol.equals("M")) {
-                result += 1000;
-            } else if (symbol.equals("D")) {
-                result += 500;
-            } else if (symbol.equals("C")) {
-                result += 100;
-            } else if (symbol.equals("L")) {
-                result += 50;
-            } else if (symbol.equals("X")) {
-                if (convertX(nextSymbol)) {
-                    i++;
-                };
-            } else if (symbol.equals("V")) {
-                result += 5;
-            } else if (symbol.equals("I")) {
-                if (convertI(nextSymbol)) {
-                    i++;
-                };
-            }
+            result +=  switch (symbol) {
+                case "M" -> 1000;
+                case "D" -> 500;
+                case "C" -> 100;
+                case "L" -> 50;
+                case "X" -> convertX(nextSymbol);
+                case "V" -> 5;
+                case "I" -> convertI(nextSymbol);
+                default -> 0;
+            };
         }
         return result;
     }
 
-    private boolean convertX(String nextSymbol) {
+    private int convertX(String nextSymbol) {
         return switch (nextSymbol) {
-            case "L" -> {
-                result += 40;
-                yield true;
-            }
-            case "C" -> {
-                result += 90;
-                yield true;
-            }
-            default -> {
-                result += 10;
-                yield false;
-            }
+            case "L" -> -10;
+            case "C" -> -10;
+            default -> 10;
         };
     }
 
-    private boolean convertI(String nextSymbol) {
+    private int convertI(String nextSymbol) {
         return switch (nextSymbol) {
-            case "V" -> {
-                result += 4;
-                yield true;
-            }
-            case "X" -> {
-                result += 9;
-                yield true;
-            }
-            default -> {
-                result += 1;
-                yield false;
-            }
+            case "V" -> -1;
+            case "X" -> -1;
+            default -> 1;
         };
     }
 
