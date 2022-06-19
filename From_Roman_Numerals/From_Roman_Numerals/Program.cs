ac138a93b65422a2cc3c982aad212c5c167d575a
Rosin, Stephan <stephan.rosin@adesso.ch>
2022-06-19 13:50:20 +0200

    .

--- From_Roman_Numerals/From_Roman_Numerals/Program.cs
+++ From_Roman_Numerals/From_Roman_Numerals/Program.cs
@@ -6,7 +6,7 @@ namespace From_Roman_Numerals
     internal class Program
     {
         // Dictionary for all valid Numerals
-        static Dictionary<char, int> numerals = new Dictionary<char, int>()
+        static Dictionary<char, int> validNumerals = new Dictionary<char, int>()
         {
             { 'I', 1 },
             { 'V', 5 },
@@ -23,15 +23,15 @@ namespace From_Roman_Numerals
             {
                 string numeralInput; // Input of numeral to evaluate
 
-                Console.WriteLine("Accepted Numerals: ");
-                foreach (KeyValuePair<char, int> numeral in numerals)
+                Console.WriteLine("Accepted numerals: ");
+                foreach (KeyValuePair<char, int> numeral in validNumerals)
                 {
                     Console.WriteLine(numeral.Key + " - " + numeral.Value);
                 }
             
                 do
                 {
-                    Console.WriteLine("Enter your Roman Numeral: ");
+                    Console.WriteLine("Enter your Roman numeral: ");
                     numeralInput = Console.ReadLine();
                     Console.WriteLine("Result: " + TranslateNumerals(numeralInput));
                 } while (numeralInput != String.Empty);
@@ -51,7 +51,7 @@ namespace From_Roman_Numerals
                 // Iterate through input
                 foreach (char singleNumeral in numeralInput)
                 {
-                    if (numerals.ContainsKey(singleNumeral))
+                    if (validNumerals.ContainsKey(singleNumeral))
                     {
                         // Check if this is the first numeral. If yes, just add it to result
                         if (lastSingleNumeral != 'z')
@@ -60,19 +60,19 @@ namespace From_Roman_Numerals
                             // example : "IV" should be 4
                             // as "I" was added to the result in the last cycle (+1), it is now clear that it was meant to be substracted from the current one,
                             // so it has to be substracted twice
-                            if (numerals[lastSingleNumeral] < numerals[singleNumeral])
+                            if (validNumerals[lastSingleNumeral] < validNumerals[singleNumeral])
                             {
-                                result = result - numerals[lastSingleNumeral] - numerals[lastSingleNumeral] + numerals[singleNumeral];
+                                result = result - validNumerals[lastSingleNumeral] - validNumerals[lastSingleNumeral] + validNumerals[singleNumeral];
                             }
                             // Otherwise just add the result
                             else
                             {
-                                result += numerals[singleNumeral];
+                                result += validNumerals[singleNumeral];
                             }
                         }
                         else
                         {
-                            result += numerals[singleNumeral];
+                            result += validNumerals[singleNumeral];
                         }
                         // Set current numeral as "last numeral" to use in the next cycle
                         lastSingleNumeral = singleNumeral;
