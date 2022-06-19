4151a9283eae0ef55f4d64647098eb587ba4c61b
Kubilay Kenan Dag <Kubilay-Kenan.dag@adesso.ch>
2022-06-19 16:11:15 +0200

    implement FizzBuzz

---
+++ Exam2/Exam2/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Exam2
+{
+	public class Program
+	{
+		private static IValidator _validatorService;
+
+		public static void Main(string[] args)
+		{
+			var serviceProvider = new ServiceCollection()
+						.AddSingleton<IValidator, Validator>()
+						.BuildServiceProvider();
+
+			_validatorService = serviceProvider.GetService<IValidator>();
+
+			Console.WriteLine("Start Application at: " + DateTime.UtcNow);
+
+			HandleFizzBuzz();
+
+			Console.WriteLine("End Application at: " + DateTime.UtcNow);
+
+		}
+
+		private static void HandleFizzBuzz()
+		{
+			for (int i = 1; i <= 100; i++)
+			{
+				var validationResult = _validatorService.ValidateCount(i);
+
+				if (validationResult.IsFizz && validationResult.IsBuzz)
+				{
+					LogCounter("FizzBuzz");
+				}
+				else if (validationResult.IsFizz)
+				{
+					LogCounter("Fizz");
+				}
+				else if (validationResult.IsBuzz)
+				{
+					LogCounter("Buzz");
+				}
+				else
+				{
+					LogCounter(i.ToString());
+				}
+			}
+		}
+
+		private static void LogCounter(string counter)
+		{
+			Console.WriteLine(counter);
+		}
+
+	}
+}
---
+++ Exam2/Exam2/Validator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2
+{
+	public class Validator : IValidator
+	{
+		public ValidationResult ValidateCount(int currentCount)
+		{
+			var result = new ValidationResult();
+
+			var isFizz = currentCount % 3 == 0;
+			var isBuzz = currentCount % 5 == 0;
+
+			if (isFizz && isBuzz)
+			{
+				result.IsFizz = true;
+				result.IsBuzz = true;
+			}
+			else if (isFizz)
+			{
+				result.IsFizz = true;
+			}
+			else if (isBuzz)
+			{
+				result.IsBuzz = true;
+			}
+
+			return result;
+		}
+	}
+}
