c57146f01d9f117a281118a14568ee3da093a3d0
janosh.suter <Cheat2007$>
2022-06-19 16:33:28 +0200

    added FizzBuzz classes and tests

---
+++ fizz.buzz/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 101; i++) {
+            printFizzBuzzNumber(i);
+        }
+    }
+
+    private static void printFizzBuzzNumber(int i) {
+        System.out.println(FizzBuzz.replaceIfFizzBuzzNumber(i));
+    }
+}
---
+++ fizz.buzz/test/FizzBuzzTest.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+public class FizzBuzzTest {
+    @Test
+    void testFizzBuzzWithDivideableBy5and3() {
+        assertEquals(FizzBuzz.replaceIfFizzBuzzNumber(15), "FizzBuzz");
+    }
+
+    @Test
+    void testFizzBuzzWithDivideableBy3() {
+        assertEquals(FizzBuzz.replaceIfFizzBuzzNumber(21), "Fizz");
+        assertEquals(FizzBuzz.replaceIfFizzBuzzNumber(9), "Fizz");
+    }
+
+    @Test
+    void testFizzBuzzWithDivideableBy5() {
+        assertEquals(FizzBuzz.replaceIfFizzBuzzNumber(20), "Buzz");
+        assertEquals(FizzBuzz.replaceIfFizzBuzzNumber(455), "Buzz");
+    }
+
+    @Test
+    void testFizzBuzzNormal() {
+        assertEquals(FizzBuzz.replaceIfFizzBuzzNumber(22), "22");
+    }
+}
