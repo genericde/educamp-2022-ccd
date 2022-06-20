b5a18cd0a624f64c7d35284d361b842cc26beccd
rcasutt <casutti@gmail.com>
2022-06-20 11:42:12 +0200

    introduce isFizz and isBuzz functions

--- clean-code/src/main/java/ch/adesso/educamp/FizzBuzzConverter.java
+++ clean-code/src/main/java/ch/adesso/educamp/FizzBuzzConverter.java
@@ -7,15 +7,23 @@ public class FizzBuzzConverter {
 
     static String[] convert(String[] numbersInRange) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (isFizz(i) && isBuzz(i)) {
                 numbersInRange[i-1] = FIZZ + BUZZ;
-            } else if (i % 3 == 0) {
+            } else if (isFizz(i)) {
                 numbersInRange[i-1] = FIZZ;
-            } else if (i % 5 == 0) {
+            } else if (isBuzz(i)) {
                 numbersInRange[i-1] = BUZZ;
             }
         }
         return numbersInRange;
+    }
+
+    private static boolean isFizz(int i) {
+        return i % 3 == 0;
+    }
+
+    private static boolean isBuzz(int i) {
+        return i % 5 == 0;
     }
 
 }
