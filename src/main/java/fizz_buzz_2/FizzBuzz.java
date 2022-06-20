adfa7aebd11d1eead3d4b982fe7d176417a03171
Nino Nufrio <nino.nufrio@adesso.ch>
2022-06-20 11:22:13 +0200

    fizzbuzz2 added

--- src/main/java/fizz_buzz_2/FizzBuzz.java
+++ src/main/java/fizz_buzz_2/FizzBuzz.java
@@ -2,8 +2,8 @@ package fizz_buzz_2;
 
 public class FizzBuzz {
     public void fizzBuzz() {
-        var numberList = RangeGenerator.generate();
-        var fizzBuzz = FizzBuzzConverter.convertFizzBuzz(numberList);
+        var numbers = RangeGenerator.generate();
+        var fizzBuzz = FizzBuzzConverter.convertFizzBuzz(numbers);
         Printer.printFizzBuzz(fizzBuzz);
     }
 }
---
+++ src/main/test/fizz_buzz_2/FizzBuzzConverterTest.java
@@ -0,0 +1,4 @@
+package fizz_buzz_2;
+
+public class FizzBuzzConverterTest {
+}
