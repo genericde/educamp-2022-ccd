0b2f0c572aeb265477bf33e044aea1864eadd214
Nino Nufrio <nino.nufrio@adesso.ch>
2022-06-20 11:33:58 +0200

    fizzbuzz2 added

--- src/main/java/fizz_buzz_2/FizzBuzz.java
+++ src/main/java/fizz_buzz_2/FizzBuzz.java
@@ -4,6 +4,6 @@ public class FizzBuzz {
     public void fizzBuzz() {
         var numbers = RangeGenerator.generate();
         var fizzBuzz = FizzBuzzConverter.convert(numbers);
-        Printer.printFizzBuzz(fizzBuzz);
+        Printer.print(fizzBuzz);
     }
 }
--- src/main/java/fizz_buzz_2/Printer.java
+++ src/main/java/fizz_buzz_2/Printer.java
@@ -3,13 +3,9 @@ package fizz_buzz_2;
 import java.util.List;
 
 public class Printer {
-    public static void printFizzBuzz(List<String> fizzBuzz) {
-        for (var fizzBuzzLine : fizzBuzz) {
-            printFizzBuzzLine(fizzBuzzLine);
+    public static void print(List<String> texts) {
+        for (var text : texts) {
+            System.out.println(text);
         }
-    }
-
-    private static void printFizzBuzzLine(String fizzBuzzLine) {
-        System.out.println(fizzBuzzLine);
     }
 }
