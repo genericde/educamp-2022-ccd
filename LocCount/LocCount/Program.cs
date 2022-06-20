838fd4db8640be92a80831c2456b07a76c29b801
Rosin, Stephan <stephan.rosin@adesso.ch>
2022-06-20 15:50:13 +0200

    .

--- LocCount/LocCount/Program.cs
+++ LocCount/LocCount/Program.cs
@@ -4,9 +4,11 @@ namespace LocCount
 {
     internal class Program
     {
+
+        private static readonly string Filepath = @"C:\Users\Stephan.Rosin\source\repos\educamp-2022-ccd\From_Roman_Numerals\From_Roman_Numerals\Program.cs";
         static void Main(string[] args)
         {
-            LocCount.Start(@"C:\Users\Stephan.Rosin\source\repos\educamp-2022-ccd\From_Roman_Numerals\From_Roman_Numerals\Program.cs");
+            LocCount.Start(Filepath);
             Console.ReadKey();
         }
     }
