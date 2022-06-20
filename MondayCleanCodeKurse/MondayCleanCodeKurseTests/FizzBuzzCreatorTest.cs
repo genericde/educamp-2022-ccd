4024aae86e5ef06cf8b13822fd62170f26450b44
Tracy Ragni <tracy.ragni@adesso.ch>
2022-06-20 11:24:06 +0200

    Added test

--- MondayCleanCodeKurse/MondayCleanCodeKurseTests/FizzBuzzCreatorTest.cs
+++ MondayCleanCodeKurse/MondayCleanCodeKurseTests/FizzBuzzCreatorTest.cs
@@ -13,10 +13,21 @@ namespace MondayCleanCodeKurseTests
         [InlineData(5, "Buzz")]
         [InlineData(3, "Fizz")]
         [InlineData(7, "7")]
-        public void FizzBuzzConverter_shoukd_convert_data(int data, string expected)
+        public void FizzBuzzConverter_should_convert_data(int data, string expected)
         {
             var result = Sut.FizzBuzzConverter(data);
             Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(100)]
+        [InlineData(1500)]
+        [InlineData(0)]
+        public void RangeCreator_should_create_range(int size)
+        {
+            var result = Sut.RangeCreator(size);
+            Assert.Equal(result.Count(),size);
         }
     }
 }
