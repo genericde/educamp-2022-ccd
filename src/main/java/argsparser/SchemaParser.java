7ba6b8c70767b955b5f242f618c6025a7ec35a83
janosh.suter <Cheat2007$>
2022-06-21 16:00:14 +0200

    refactoring

--- src/main/java/argsparser/SchemaParser.java
+++ src/main/java/argsparser/SchemaParser.java
@@ -3,19 +3,23 @@ package main.java.argsparser;
 public class SchemaParser {
 
     public static ParsedSchema parse(String schemaSpec) {
-        String[] splitedSchemas = schemaSpec.split(",");
+        String[] splitedSchemaParts = schemaSpec.split(",");
         ParsedSchema parsedSchema = new ParsedSchema();
-        for (String schema : splitedSchemas){
-            if (schema.contains("?")){
-                parsedSchema.schemaLetterForBoolean = schema.split("/?")[0];
-            } else if (schema.contains("*")) {
-                parsedSchema.schemaLetterForString = schema.split("/*")[0];
-            } else if (schema.contains("#")) {
-                parsedSchema.schemaLetterForInteger = schema.split("#")[0];
+        for (String schemaPart : splitedSchemaParts){
+            if (schemaPart.contains("?")){
+                parsedSchema.schemaLetterForBoolean = getSchemaLetterForBoolean(schemaPart, "/?");
+            } else if (schemaPart.contains("*")) {
+                parsedSchema.schemaLetterForString = getSchemaLetterForBoolean(schemaPart, "/*");
+            } else if (schemaPart.contains("#")) {
+                parsedSchema.schemaLetterForInteger = getSchemaLetterForBoolean(schemaPart, "#");
             } else {
                 throw new IllegalArgumentException("Invalid Schema Arguments");
             }
         }
         return parsedSchema;
+    }
+
+    private static String getSchemaLetterForBoolean(String schemaPart, String regex) {
+        return schemaPart.split(regex)[0];
     }
 }
